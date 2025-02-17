fda8f6194b655c542b617d25824553488c5d54f4
Kerick Howlett <8661181+KerickHowlett@users.noreply.github.com>
2025-02-17 13:43:21 -0500

    chore(core): set up dev environment

--- src/main.ts
+++ src/main.ts
@@ -1,19 +1,18 @@
-/**
- * This is not a production server yet!
- * This is only a minimal backend to get started.
- */
-
 import { Logger } from '@nestjs/common';
 import { NestFactory } from '@nestjs/core';
+
 import { AppModule } from './app/app.module';
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
-  const globalPrefix = 'api';
-  app.setGlobalPrefix(globalPrefix);
-  const port = process.env.PORT || 3000;
-  await app.listen(port);
-  Logger.log(`🚀 Application is running on: http://localhost:${port}/${globalPrefix}`);
+    const app = await NestFactory.create(AppModule);
+
+    const GLOBAL_PREFIX = 'api' as const;
+    app.setGlobalPrefix(GLOBAL_PREFIX);
+
+    const PORT = +process.env.PORT || 3000;
+    await app.listen(PORT);
+
+    Logger.log(`🚀 Application is running on: http://localhost:${PORT}/${GLOBAL_PREFIX}`);
 }
 
 bootstrap();
