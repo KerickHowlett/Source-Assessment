d126bd16062629e9fdb7c2e58b697d9d60b2763f
Kerick Howlett <8661181+KerickHowlett@users.noreply.github.com>
2025-02-17 20:50:43 -0500

    chore(core): straighten out parallel db systems

--- e2e/src/server/server.spec.ts
+++ e2e/src/server/server.spec.ts
@@ -1,10 +1,36 @@
+import { faker } from '@faker-js/faker';
+import { PrismaClient } from '@prisma/client';
 import axios from 'axios';
+import { range } from 'lodash';
 
 describe('GET /api', () => {
-  it('should return a message', async () => {
-    const res = await axios.get(`/api`);
+    let prisma: PrismaClient;
 
-    expect(res.status).toBe(200);
-    expect(res.data).toEqual({ message: 'Hello API' });
-  });
-})
+    beforeAll(async () => {
+        prisma = new PrismaClient();
+
+        const users = range(10).map(() => ({
+            name: faker.person.fullName(),
+            email: faker.internet.email(),
+        }));
+
+        await prisma.$connect();
+        await prisma.$transaction([prisma.user.deleteMany()]);
+        await prisma.user.createMany({
+            data: users,
+            skipDuplicates: true,
+        });
+    });
+
+    afterAll(async () => {
+        await prisma.$transaction([prisma.user.deleteMany()]);
+        await prisma.$disconnect();
+    });
+
+    it('should return a message', async () => {
+        const response = await axios.get(`http://localhost:3000/api`);
+
+        expect(response.status).toEqual(200);
+        expect(response.data).toEqual({ message: 'Hello API' });
+    });
+});
--- src/main.ts
+++ src/main.ts
@@ -1,4 +1,4 @@
-import { Logger } from '@nestjs/common';
+import { Logger, ValidationPipe } from '@nestjs/common';
 import { NestFactory } from '@nestjs/core';
 
 import { AppModule } from './app/app.module';
@@ -8,6 +8,12 @@ async function bootstrap() {
 
     const GLOBAL_PREFIX = 'api' as const;
     app.setGlobalPrefix(GLOBAL_PREFIX);
+
+    app.useGlobalPipes(
+        new ValidationPipe({
+            whitelist: true,
+        }),
+    );
 
     const PORT = +process.env.PORT || 3000;
     await app.listen(PORT);
